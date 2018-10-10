2c0b86b1f818cb17fc0fe69cb4307e3c27f62585
Jeffrey Matthews <jmatthews@URBANSCIENCE.com>
2018-10-10 12:32:12 -0400

    Initial commit

---
+++ graphtools.py
@@ -0,0 +1,36 @@
+'''A basic set of functions for dealing with python graphs represented by a dictionary of nodes to adjacency lists.
+For examp graph = {'a': ['b'], 'b': ['a', 'c'], 'c': ['c']}
+'''
+
+import itertools
+
+def permute_vertices(graph):
+    return itertools.permutations(graph.keys())
+
+def graph_import():
+    vertices = list(sorted(input('Enter vertices: ')))
+    graph = {}
+    for i in vertices:
+        connections = list(sorted(input('Enter connections to ' + i + ': ')))
+        graph[i] = connections
+    return graph
+
+def is_edge(graph, a, b):
+    return b in graph[a]
+
+def is_valid_path(graph, path):
+    for i in range(len(path) - 1):
+        if not is_edge(graph, path[i], path[i + 1]):
+            return False
+    return True
+
+def is_valid_cycle(graph, cycle):
+    return is_edge(graph, cycle[-1], cycle[0]) and is_valid_path(graph, cycle)
+
+def hamiltonian_cycles(graph):
+    result = []
+    for p in permute_vertices(graph):
+        if is_valid_cycle(graph, p):
+            result.append(p)
+    return result
+
